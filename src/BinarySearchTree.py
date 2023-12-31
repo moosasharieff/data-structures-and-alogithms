ae389c658dd1a47bce8de8c224fdfff077c74a3a
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2023-12-31 11:24:06 +0000

    #1 Adding insert method for Binary Search Tree Data Structure.

--- src/BinarySearchTree.py
+++ src/BinarySearchTree.py
@@ -14,3 +14,31 @@ class BinarySearchTree:
         Initilizes empty tree. Nodes can be added from insert method.
         """
         self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+
+        # check if root is empty
+        if self.root is None:
+            self.root = new_node
+            return True
+
+        # Traverse through the tree
+        temp = self.root
+        while True:
+            # check if new_node's value is already present
+            if new_node.value == temp.value:
+                return False
+
+            # Moving inside the tree
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
